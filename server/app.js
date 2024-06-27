f9c0416fb997d3e0416be8e5a456e945d7fc0e16
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-06-27 18:10:35 +0300

    Renew server

---
+++ server/app.js
@@ -0,0 +1,73 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const carriersRouter = require('./routes/carriers');
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Set up mongoose connection
+mongoose.set('strictQuery', false);
+
+const mongoDB = process.env.mongoUri;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log('Connected to database!!');
+}
+
+// Cors
+const allowedOrigins = [process.env.clientUrl];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use('/carriers', carriersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
