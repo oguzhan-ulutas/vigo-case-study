7a2dc907ff8dedeb0a4508b0dfba573454fcfa93
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-06-27 18:50:05 +0300

    fix leaflet errors

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,13 +3,26 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Button from "@mui/material/Button";
 import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 delete L.Icon.Default.prototype._getIconUrl;
+
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
 });
+
+// // Fix for missing default icon issue in Leaflet
+// delete L.Icon.Default.prototype._getIconUrl;
+
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: "../assets/marker-icon-2x.png",
+//   iconUrl: "../assets/marker-icon.png",
+//   shadowUrl: "../assets/marker-shadow.png",
+// });
 
 const Map = ({ setUserMessage, setUserLocation, userLocation, setCarrier }) => {
   const [carriers, setCarriers] = useState([]);
